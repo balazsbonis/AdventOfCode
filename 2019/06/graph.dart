5f25a6546f6fe0f6f5a2ec2cd5f5ae896c4f47f1
Balázs Bónis <balazs.bonis@greenfinch.ie>
2019-12-06 15:42:42 +0000

    [DAY6] - Refactored graph so it's reusable

--- 2019/06/graph.dart
+++ 2019/06/graph.dart
@@ -3,8 +3,12 @@ library graph;
 class Node {
   final String name;
   final depth;
+
   List<Node> nodes;
   Node parent;
+
+  Set<List<String>> _inputs;
+  List<Node> _visitedNodes = new List<Node>();
 
   Node(this.name, this.depth, [this.parent]) {
     nodes = new List<Node>();
@@ -16,16 +20,74 @@ class Node {
   @override
   int get hashCode => name.hashCode;
 
-  Node findDepthFirst(Node node) {
-    if (this == node) return this;
-    for (var n in nodes) {
-      return findDepthFirst(n);
-    }
-    return null;
-  }
-
   @override
   String toString() {
     return "${name} -> (${nodes.join("\n\t")})";
+  }
+
+  int depthFirstCrawl() {
+    return _depthFirstCrawl(this);
+  }
+
+  Node findNode(String name) {
+    return _findNode(this, name);
+  }
+
+  void buildTree(Set<List<String>> inputs) {
+    _inputs = inputs;
+    this.nodes = _buildTree(this, 1);
+  }
+
+  int getDistance(String source, String target){
+    var sourceNode = findNode(source);
+    return _getDistance(sourceNode, target, -1); // initial node doesn't count
+  }
+
+  int _getDistance(Node source, String target, int distance) {
+    if (source.name == target) {
+      return distance -
+          1; // we already reached the destination, don't need to add 1 more
+    }
+
+    _visitedNodes.add(source);
+    // first check all the children
+    for (var n in source.nodes.where((f) => !_visitedNodes.contains(f))) {
+      //print("v ${source.name} -> ${n.name}; $distance");
+      return _getDistance(n, target, distance + 1);
+    }
+
+    if (!_visitedNodes.contains(source.parent)) {
+      // check the parent
+      //print("^ ${source.name} -> ${source.parent.name}; $distance");
+      return _getDistance(source.parent, target, distance + 1);
+    }
+    // dead-end, go back all the way
+    return _getDistance(source.parent, target, distance - 1);
+  }
+
+  static int _depthFirstCrawl(Node root) {
+    if (root.nodes.length == 0) return root.depth;
+    var result = root.depth;
+    root.nodes.forEach((f) => result += _depthFirstCrawl(f));
+    return result;
+  }
+
+  static Node _findNode(Node root, String name) {
+    if (root.name == name) return root;
+    for (var n in root.nodes) {
+      var found = _findNode(n, name);
+      if (found != null) {
+        return found;
+      }
+    }
+  }
+
+  List<Node> _buildTree(Node root, int depth) {
+    var inputsToAdd = _inputs.where((f) => f[0] == root.name);
+    inputsToAdd.forEach((f) => root.nodes.add(new Node(f[1], depth, root)));
+    for (var n in root.nodes) {
+      n.nodes = _buildTree(n, depth + 1);
+    }
+    return root.nodes;
   }
 }
--- 2019/06/main.dart
+++ 2019/06/main.dart
@@ -7,9 +7,7 @@ class Puzzle {
   List<String> input = new List<String>();
   dynamic solution1;
   dynamic solution2;
-
   Set<List<String>> inputToProcess;
-  List<Node> visitedNodes = new List<Node>();
 
   Puzzle() {}
 
@@ -30,56 +28,9 @@ class Puzzle {
     var rootNode =
         new Node(rootNames.firstWhere((f) => !nodeNames.contains(f)), 0);
 
-    rootNode.nodes = buildTree(rootNode, 1);
-    solution1 = depthFirstCrawl(rootNode);
-    solution2 = getDistance(findNode(rootNode, "YOU"), "SAN", -1); // initial node doesn't count
-
-    print(rootNode);
-  }
-
-  List<Node> buildTree(Node root, int depth) {
-    var inputsToAdd = inputToProcess.where((f) => f[0] == root.name);
-    inputsToAdd.forEach((f) => root.nodes.add(new Node(f[1], depth, root)));
-    for (var n in root.nodes) {
-      n.nodes = buildTree(n, depth + 1);
-    }
-    return root.nodes;
-  }
-
-  int depthFirstCrawl(Node root) {
-    if (root.nodes.length == 0) return root.depth;
-    var result = root.depth;
-    root.nodes.forEach((f) => result += depthFirstCrawl(f));
-    return result;
-  }
-
-  Node findNode(Node root, String name) {
-    if (root.name == name) return root;
-    for (var n in root.nodes) {
-      var found = findNode(n, name);
-      if (found != null) {
-        return found;
-      }
-    }
-  }
-
-  int getDistance(Node source, String target, int distance) {
-    if (source.name == target) {
-      return distance - 1; // we already reached the destination, don't need to add 1 more
-    }
-
-    visitedNodes.add(source);
-    // first check all the children
-    for (var n in source.nodes.where((f) => !visitedNodes.contains(f))) {
-      print("v ${source.name} -> ${n.name}; $distance");
-      return getDistance(n, target, distance + 1);
-    }
-
-    if (!visitedNodes.contains(source.parent)) {
-      print("^ ${source.name} -> ${source.parent.name}; $distance");
-      return getDistance(source.parent, target, distance + 1);
-    }
-    return getDistance(source.parent, target, distance - 1);
+    rootNode.buildTree(inputToProcess);
+    solution1 = rootNode.depthFirstCrawl();
+    solution2 = rootNode.getDistance("YOU", "SAN"); 
   }
 }
 
