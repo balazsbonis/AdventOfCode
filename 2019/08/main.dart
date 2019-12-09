cb8516ab8873fd1fc7a4d8e398fe16c666763bbf
Balázs Bónis <balazs.bonis@greenfinch.ie>
2019-12-09 08:48:46 +0000

    Add day 8,9,10

---
+++ 2019/08/main.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+class Puzzle {
+  final inputFile = new File(".\\08\\input.txt");
+  List<String> input = new List<String>();
+  dynamic solution1;
+  dynamic solution2;
+
+  Puzzle() {}
+
+  List<String> parseInputByLine() {
+    return input = inputFile.readAsStringSync().trim().split("\n");
+  }
+
+  List<String> parseInputBySeparator({String separator = ","}) {
+    return input = inputFile.readAsStringSync().trim().split(separator);
+  }
+
+  void solve() {}
+}
+
+void main() {
+  Puzzle puzzle = new Puzzle();
+
+  Stopwatch stopwatch = new Stopwatch();
+  stopwatch.start();
+  puzzle.solve();
+  stopwatch.stop();
+
+  print("Solution to part1: ${puzzle.solution1}");
+  print("Solution to part2: ${puzzle.solution2}");
+  print("Execution took ${stopwatch.elapsed}");
+}
