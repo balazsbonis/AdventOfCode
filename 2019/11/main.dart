474b7ca8a4194b6f9e7936559a14ff0a539fc33b
Balázs Bónis <balazs.bonis@greenfinch.ie>
2019-12-14 22:32:00 +0100

    [DAY11] - Solved

--- 2019/11/main.dart
+++ 2019/11/main.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
+import 'package:image/image.dart';
+
+import '../base/intcode_compiler.dart';
 
 class Puzzle {
   final inputFile = new File(".\\11\\input.txt");
   List<String> input = new List<String>();
   dynamic solution1;
   dynamic solution2;
+  List<int> params = [1]; // set to 0 for part 1, 1 for part 2
+  var posX = 0;
+  var posY = 0;
+  bool modeBit = false; // false = color, true = direction
+  String direction = "U";
+  Map hull = new Map();
 
   Puzzle() {}
 
@@ -16,9 +25,103 @@ class Puzzle {
     return input = inputFile.readAsStringSync().trim().split(separator);
   }
 
-  void solve() {}
+  void solve() {
+    var memory =
+        parseInputBySeparator().map((String s) => int.parse(s)).toList();
+
+    hull["$posX,$posY"] = 0;
+    new Runner(memory, params)
+      ..reset()
+      ..setOutputCallback((res) => paintHull(res))
+      ..run();
+    solution1 = hull.length;
+
+    paintHullToFile();
+  }
+
+  void paintHull(int result) {
+    if (!modeBit) {
+      // paint
+      hull["$posX,$posY"] = result;
+    } else {
+      // direction
+      if (result == 0) {
+        // left
+        switch (direction) {
+          case "U":
+            direction = "L";
+            break;
+          case "L":
+            direction = "D";
+            break;
+          case "D":
+            direction = "R";
+            break;
+          case "R":
+            direction = "U";
+            break;
+        }
+      } else {
+        // right
+        switch (direction) {
+          case "U":
+            direction = "R";
+            break;
+          case "R":
+            direction = "D";
+            break;
+          case "D":
+            direction = "L";
+            break;
+          case "L":
+            direction = "U";
+            break;
+        }
+      }
+      moveRobot();
+      params.add(hull["$posX,$posY"]);
+    }
+    modeBit = !modeBit;
+    print("$posX,$posY");
+  }
 
   void solveTest() {}
+
+  void moveRobot() {
+    switch (direction) {
+      case "U":
+        posY++;
+        break;
+      case "R":
+        posX++;
+        break;
+      case "D":
+        posY--;
+        break;
+      case "L":
+        posX--;
+        break;
+    }
+    if (!hull.containsKey("$posX,$posY")) {
+      hull["$posX,$posY"] = 0;
+    } else {
+      print("BEEN HERE $posX,$posY");
+    }
+  }
+
+  void paintHullToFile() {
+    Image image = Image(200, 200);
+    fill(image, getColor(0, 0, 0));
+    hull.forEach((k, v) => putPixel(image, k, v));
+    File('output11.png').writeAsBytesSync(encodePng(image));
+  }
+
+  putPixel(Image image, k, v) {
+    if (v.toString() == "1") {
+      var coords = k.toString().split(",").map((n) => int.parse(n)).toList();
+      drawPixel(image, 100-coords[0], 100-coords[1], getColor(255,255,255));
+    }
+  }
 }
 
 void main() {
