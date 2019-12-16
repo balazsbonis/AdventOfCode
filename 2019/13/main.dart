bee221608527b444709565d260255c19b3d2e6cb
Balázs Bónis <balazs.bonis@greenfinch.ie>
2019-12-16 02:28:54 +0100

    [DAY13] - Part 1 done

--- 2019/13/main.dart
+++ 2019/13/main.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 
+import '../base/intcode_compiler.dart';
+
 class Puzzle {
   final inputFile = new File(".\\13\\input.txt");
   List<String> input = new List<String>();
   dynamic solution1;
   dynamic solution2;
+
+  Map screen = new Map();
+  int curOutType = 0; // 0 = x, 1 = y, 2 = object
+  int curX, curY;
 
   Puzzle() {}
 
@@ -16,9 +22,44 @@ class Puzzle {
     return input = inputFile.readAsStringSync().trim().split(separator);
   }
 
-  void solve() {}
+  void solve() {
+    var memory =
+        parseInputBySeparator().map((String s) => int.parse(s)).toList();
+
+    var params = [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    memory[0] = 2;
+    new Runner(memory, params)
+      ..reset()
+      ..setOutputCallback((res) => paintScreen(res))
+      ..run();
+
+    //printScreen();
+    //screen.removeWhere((k,v) => v != 2);
+    //solution1 = screen.length; //320
+  }
 
   void solveTest() {}
+
+  void paintScreen(res) {
+    switch (curOutType) {
+      case 0:
+        curX = res;
+        break;
+      case 1:
+        curY = res;
+        break;
+      default:
+        screen["$curX,$curY"] = res;
+        break;
+    }
+
+    curOutType++;
+    if (curOutType == 3) {
+      curOutType = 0;
+    }
+  }
+
+  
 }
 
 void main() {
