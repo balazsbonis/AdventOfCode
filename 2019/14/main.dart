02f54f37d8c35d0e212dda0664890b1ba682e757
Balázs Bónis <balazs.bonis@greenfinch.ie>
2019-12-20 17:04:43 +0000

    [DAY14] - Start

--- 2019/14/main.dart
+++ 2019/14/main.dart
@@ -1,12 +1,46 @@
 import 'dart:io';
 
+class Element {
+  String name;
+  int quantity;
+
+  Element(this.name, this.quantity);
+
+  Element.fromString(String input) {
+    var parts = input.split(" ");
+    quantity = int.parse(parts[0]);
+    name = parts[1];
+  }
+}
+
+class Reaction {
+  Element result;
+  List<Element> reagents;
+
+  Reaction(this.result, this.reagents);
+
+  Reaction.fromString(String input) {
+    reagents = new List<Element>();
+    var parts = input.split("=>");
+    var elementParts = parts[0].trim();
+    var resultPart = parts[1].trim();
+    result = new Element.fromString(resultPart);
+    for (var p in elementParts.split(",")) {
+      reagents.add(new Element.fromString(p.trim()));
+    }
+  }
+}
+
 class Puzzle {
   final inputFile = new File(".\\14\\input.txt");
   List<String> input = new List<String>();
   dynamic solution1;
   dynamic solution2;
+  List<Reaction> reactions;
 
-  Puzzle() {}
+  Puzzle() {
+    reactions = new List<Reaction>();
+  }
 
   List<String> parseInputByLine() {
     return input = inputFile.readAsStringSync().trim().split("\n");
@@ -18,7 +52,25 @@ class Puzzle {
 
   void solve() {}
 
-  void solveTest() {}
+  void solveTest() {
+    input = [
+      "9 ORE => 2 A",
+      "8 ORE => 3 B",
+      "7 ORE => 5 C",
+      "3 A, 4 B => 1 AB",
+      "5 B, 7 C => 1 BC",
+      "4 C, 1 A => 1 CA",
+      "2 AB, 3 BC, 4 CA => 1 FUEL"
+    ];
+    for (var i in input) {
+      reactions.add(new Reaction.fromString(i));
+    }
+    // find the one reaction with fuel
+    var ending = reactions.where((x) => x.result.name == "FUEL").first;
+    print("M");
+  }
+
+  
 }
 
 void main() {
@@ -26,7 +78,7 @@ void main() {
 
   Stopwatch stopwatch = new Stopwatch();
   stopwatch.start();
-  puzzle.solve();
+  puzzle.solveTest();
   stopwatch.stop();
 
   print("Solution to part1: ${puzzle.solution1}");
