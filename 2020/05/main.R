9a33b94c4179dac2f6c938e10b4f550c6b1bf556
Balázs Bónis <balazs.bonis@greenfinch.ie>
2020-12-05 09:23:35 +0000

    [2020-D5] - Part 2 programmatically

--- 2020/05/main.R
+++ 2020/05/main.R
@@ -2,6 +2,7 @@ data <-
   scan("c:\\Work\\Code\\AdventOfCode\\2020\\05\\input.txt",
        character())
 
-a <- gsub("([FL])", 0, gsub("([BR])", 1, data))
+a <- sort(strtoi(gsub("([FL])", 0, gsub("([BR])", 1, data)), base = 2))
+part1 <- max(a)
+part2 <- setdiff(seq(min(b), part1), a)
 
-part1 <- max(strtoi(a, base = 2))
