36906c92b51586cade83160eb40854758f329551
Balázs Bónis <balazs.bonis@greenfinch.ie>
2020-12-07 16:15:14 +0000

    [2020-D7] - Part 2

--- 2020/07/main.R
+++ 2020/07/main.R
@@ -2,10 +2,11 @@ library(dplyr)
 library(igraph)
 
 input <-
-  scan("c:\\Work\\Code\\AdventOfCode\\2020\\07\\test.txt",
+  scan("c:\\Work\\Code\\AdventOfCode\\2020\\07\\input.txt",
        character(),
        sep = "\n")
-parts <- strsplit(gsub("s, ", "", gsub("s contain ", "", input)), " bag")
+parts <-
+  strsplit(gsub("s, ", "", gsub("s contain ", "", input)), " bag")
 sparse <- matrix(
   0,
   nrow = length(parts),
@@ -33,3 +34,17 @@ g <- graph_from_adjacency_matrix(sparse, weighted = TRUE)
 d <- distances(g, to = "shiny gold", mode = "out")
 # -1 because the distance to shiny gold is 0
 part1 <- length(d[is.finite(rowSums(d)),]) - 1
+
+get_bags_count <- function(node_name, multiplier) {
+  bag <- sparse[node_name, ]
+  res <- multiplier
+  for (i in 1:length(parts)) {
+    if (bag[i] > 0) {
+      res <- res + get_bags_count(i, bag[i] * multiplier)
+    }
+  }
+  return (res)
+}
+
+# -1 for shiny gold. 358 is the ordinal number of shiny gold in the input.
+part2 <- get_bags_count(358, 1) - 1
