91ba421058bf3b86f8707f95a54ffd3e1b98b2c8
Balázs Bónis <balazs.bonis@greenfinch.ie>
2020-12-12 15:32:09 +0000

    [2020-D12] - Shorten code

--- 2020/12/part2.R
+++ 2020/12/part2.R
@@ -3,7 +3,12 @@ f <- file("c:\\Work\\Code\\AdventOfCode\\2020\\12\\input.txt", "r")
 # part2
 ship <- c(0, 0)
 waypoint <- c(10, 1)
-
+movements <- list(
+  E = c(1, 0),
+  S = c(0,-1),
+  W = c(-1, 0),
+  N = c(0, 1)
+)
 while (TRUE) {
   line <- readLines(f, n = 1)
   if (length(line) == 0) {
@@ -11,27 +16,19 @@ while (TRUE) {
   }
   order <- substring(line, 1, 1)
   measure <- strtoi(substring(line, 2))
-  if (order == "N") {
-    waypoint[2] <- waypoint[2] + measure
-  }
-  else if (order == "S") {
-    waypoint[2] <- waypoint[2] - measure
-  }
-  else if (order == "E") {
-    waypoint[1] <- waypoint[1] + measure
-  }
-  else if (order == "W") {
-    waypoint[1] <- waypoint[1] - measure
+  if (order == "N" || order == "E" || order == "S" || order == "W") {
+    waypoint <- waypoint + (measure * movements[order][[1]])
   }
   else if (order == "F") {
-    ship[1] <- ship[1] + (waypoint[1] * measure)
-    ship[2] <- ship[2] + (waypoint[2] * measure)
+    ship <- ship + (waypoint * measure)
   }
   else if (order == "R" || order == "L") {
     angle <- measure * pi / 180
-    if (order == "R") angle <- angle * -1
-    M <- matrix(c(cos(angle),-sin(angle), sin(angle), cos(angle)), 2, 2)
+    if (order == "R")
+      angle <- angle * -1
+    M <-
+      matrix(c(cos(angle),-sin(angle), sin(angle), cos(angle)), 2, 2)
     waypoint <- waypoint %*% M
   }
 }
-part1 <- abs(ship[1]) + abs(ship[2])
+part2 <- abs(ship[1]) + abs(ship[2])
