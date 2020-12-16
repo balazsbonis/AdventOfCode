99ded74642cb0b9797f659f765bfa664245f6706
Balázs Bónis <balazs.bonis@greenfinch.ie>
2020-12-16 20:18:24 +0000

    [2020-D15] - Make it run quick

--- 2020/15/main.R
+++ 2020/15/main.R
@@ -1,13 +1,25 @@
 input <- c(0, 12, 6, 13, 20, 1, 17)
-limit <- 3e7
+limit <- 30000000
 numbers <- rep(0, limit)
 numbers[1:length(input)] <- input
+mylist <- rep(0, limit)
+mylist[1] <- 1
+mylist[13] <- 2
+mylist[7] <- 3
+mylist[14] <- 4
+mylist[21] <- 5
+mylist[2] <- 6
 
 for (i in (length(input) + 1):limit) {
+  if (i %% 100000 == 0)
+    print(i)
   last_no <- numbers[i - 1]
-  if (any(numbers[1:(i - 2)] == last_no)) {
-    d <- diff(tail(which(numbers[1:(i - 1)] == last_no), 2))
-    numbers[i] <- d
+  number_found <- mylist[(last_no + 1)]
+  if (number_found == 0) {
+    mylist[(last_no + 1)] <- (i - 1)
+  } else {
+    numbers[i] <- i - 1 - number_found
+    mylist[(last_no + 1)] <- (i - 1)
   }
 }
 print(numbers[limit])
