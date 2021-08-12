8e8047eb713f2a72132ef65336c6bcf0fab35c52
Balázs Bónis <balazs.bonis@greenfinch.ie>
2021-08-12 09:05:34 +0100

    [2020-22] - Reddit

--- 2020/20/main.R
+++ 2020/20/main.R
@@ -50,15 +50,8 @@ for (i in 1:(length(tiles) - 1)) {
             neighbourhood[strtoi(names(tiles[i]))] + 1
           neighbourhood[strtoi(names(tiles[j]))] <-
             neighbourhood[strtoi(names(tiles[j]))] + 1
-          print(paste(
-            names(sides_i[k]),
-            "of",
-            names(tiles[i]),
-            "matches",
-            names(sides_j[l]),
-            "of",
-            names(tiles[j])
-          ))
+          #print(paste(names(sides_i[k]), "of", names(tiles[i]), "matches",
+            #names(sides_j[l]), "of", names(tiles[j])))
         }
       }
     }
@@ -68,3 +61,19 @@ for (i in 1:(length(tiles) - 1)) {
 # strip out double counts
 neighbourhood <- neighbourhood / 2
 part1 <- prod(which(neighbourhood == 2))
+
+
+## Part 2
+# get the image and put the first corner in
+image_size <- sqrt(length(tiles)) * tile_size
+image <- matrix(".", nrow = image_size, ncol = image_size)
+first_corner <- tiles[toString(min(which(neighbourhood == 2)))]
+image[1:tile_size, 1:tile_size] <- first_corner[[1]]
+tiles_left_to_place <- names(tiles)
+tiles_left_to_place <- tiles_left_to_place[tiles_left_to_place != first_corner_name]
+tiles_placed_this_round <- c(first_corner_name)
+while (length(tiles_left_to_place) > 0){
+  tiles_placed_now <- c()
+  # find right side neighbour
+  
+}
