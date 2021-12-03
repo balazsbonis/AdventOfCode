37e4b59db39d8465bf18472af17a8bab1c6361d8
Balázs Bónis <balazs.bonis@tekenable.com>
2021-12-03 10:12:38 +0100

    [DAY02] - Turning part 1 functional

--- 2021/02/Program.fs
+++ 2021/02/Program.fs
@@ -3,16 +3,14 @@ open System.IO
 open System.Linq
 
 let part01 : int =
-    let lines = File.ReadLines("C:\\Work\\Code\\AdventOfCode\\2021\\02\\input.txt") |> Seq.map string
-    let mutable horizontalPosition = 0
-    let mutable depth = 0
-    for ll in (lines.ToList()) do
-        let spl = ll.Split(' ')
-        horizontalPosition <- 
-            if (spl.[0] = "forward") then horizontalPosition + int (spl.[1]) else horizontalPosition
-        depth <- if (spl.[0] = "down") then depth + int (spl.[1]) elif (spl.[0] = "up") then depth - int (spl.[1]) else depth
-    
-    horizontalPosition * depth
+    let lines = File.ReadLines("C:\\Work\\Code\\AdventOfCode\\2021\\02\\input.txt") |> Seq.toList
+    let position = lines |> List.filter(fun x -> x.ToString().StartsWith("forward"))
+                    |> List.map(fun x -> int (x.Replace("forward ", "")))
+                    |> List.sum
+    let depth = lines |> List.filter(fun x -> not (x.ToString().StartsWith("forward")))
+                |> List.map(fun x -> int (if (x.StartsWith("down ")) then x.Split(' ').[1] else "-" + x.Split(' ').[1]))
+                |> List.sum
+    position * depth
 
 let part02 : int =
     let lines = File.ReadLines("C:\\Work\\Code\\AdventOfCode\\2021\\02\\input.txt") |> Seq.map string
