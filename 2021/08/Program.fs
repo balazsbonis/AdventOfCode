a2700c53bd22ac7bc4d8f554ed66b749ad0d5431
Balázs Bónis <balazs.bonis@tekenable.com>
2021-12-10 14:53:30 +0100

    [DAY08] - Part 1

---
+++ 2021/08/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.IO
+
+
+[<EntryPoint>]
+let main argv =
+    let result = File.ReadLines("C:\\Work\\Code\\AdventOfCode\\2021\\08\\input.txt") 
+                    |> Seq.map(fun r -> r.Split(" | ")) |> Seq.map(fun r -> r.[1].Split(" "))
+                    |> Seq.concat |> Seq.filter(fun f -> f.Length = 2 || f.Length = 3 || f.Length = 4 || f.Length = 7)
+                    |> Seq.length
+    result
