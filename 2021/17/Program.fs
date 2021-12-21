e0fb295206d18bc6acaeb3e6e5569aaf73ca4252
Balázs Bónis <balazs.bonis@tekenable.com>
2021-12-21 11:54:24 +0100

    [DAY17&21] - Part 1

---
+++ 2021/17/Program.fs
@@ -0,0 +1,16 @@
+﻿let mutable counter = 0
+let mutable x = 1 // going up
+let mutable y = 146 // going down
+
+let validateTrajectory x y : int =
+    let traj = seq 
+                { 
+                    for i in 0..10 do 
+                        ((if (x-i>0) then (x-i) else 0),y-i)
+                } |> Seq.toList
+    let minX = traj |> List.minBy (fun (curX, curY) -> curX)
+    0
+
+let blah = validateTrajectory x y
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
---
+++ 2021/21/Program.fs
@@ -0,0 +1,28 @@
+﻿let mutable player1Position = 4
+let mutable player2Position = 1
+let mutable player1Score = 0
+let mutable player2Score = 0
+let mutable dieValue = 1
+let mutable ended = false
+
+let rollDie dieValue position : int =
+    let roll = dieValue * 3 + 3
+    let mutable newPosition = ((roll % 10) + position) % 10
+    if (newPosition = 0) then newPosition <- 10
+    newPosition
+
+while (ended = false) do
+
+    player1Position <- rollDie dieValue player1Position
+    player1Score <- player1Score + player1Position
+    dieValue <- dieValue + 3
+    if (player1Score > 999) then ended <- true
+
+    else 
+        player2Position <- rollDie dieValue player2Position
+        player2Score <- player2Score + player2Position
+        dieValue <- dieValue + 3
+    if (player2Score > 999) then ended <- true
+
+printfn "%i" ((dieValue - 1) * player2Score)
+let l = System.Console.ReadLine()
