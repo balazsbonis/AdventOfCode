5cf017acea9443ccbb709cc9ef813188a56d991b
Balázs Bónis <balazs.bonis@tekenable.com>
2022-12-02 10:54:48 +0100

    [DAY01 & 02] - done

---
+++ 2022/01/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> elves = new List<int>() {0};
+using (var str = File.OpenText("input.txt")){
+    while(!str.EndOfStream)
+    {
+        var l = str.ReadLine();
+        if (!string.IsNullOrEmpty(l)){
+            elves[elves.Count-1] += int.Parse(l);
+        } else{
+            elves.Add(0);
+        }
+    }
+}
+// part 1
+Console.WriteLine(elves.Max());
+// part 2
+Console.WriteLine(elves.OrderByDescending(x=>x).Take(3).Sum());
---
+++ 2022/02/Program.cs
@@ -0,0 +1,47 @@
+﻿int score01 = 0;
+int score02 = 0;
+using (var str = File.OpenText("input.txt")){
+    while(!str.EndOfStream)
+    {
+        var l = str.ReadLine();
+        var turn = l.Split(' ');
+        if ((turn[0] == "A" && turn[1] == "Y") || (turn[0] == "B" && turn[1] == "Z") || (turn[0] == "C" && turn[1] == "X"))
+            score01+=6;
+        if ((turn[0] == "A" && turn[1] == "X") || (turn[0] == "B" && turn[1] == "Y") || (turn[0] == "C" && turn[1] == "Z"))
+            score01+=3;
+        switch (turn[1]){
+            case "X":{
+                score01+=1;
+                switch (turn[0]) {
+                    case "A" : score02+=3; break;
+                    case "B" : score02+=1; break;
+                    case "C" : score02+=2; break;
+                } 
+                break;
+            }
+            case "Y":{
+                score01+=2; 
+                score02+=3; 
+                switch (turn[0]) {
+                    case "A" : score02+=1; break;
+                    case "B" : score02+=2; break;
+                    case "C" : score02+=3; break;
+                } 
+                break;
+            }
+            case "Z":{
+                score01+=3; 
+                score02+=6;
+                switch (turn[0]) {
+                    case "A" : score02+=2; break;
+                    case "B" : score02+=3; break;
+                    case "C" : score02+=1; break;
+                } 
+                break;
+            }
+        }
+    }
+}
+// part 1
+Console.WriteLine(score01);
+Console.WriteLine(score02);
