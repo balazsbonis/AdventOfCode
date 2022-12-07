60e06c4c463b861826d990a31c67d4f09b147211
Balázs Bónis <balazs.bonis@tekenable.com>
2022-12-07 18:27:07 +0100

    [DAY06] - again, wtf was that pt2?!

---
+++ 2022/06/Program.cs
@@ -0,0 +1,12 @@
+﻿// var input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".ToCharArray();
+var input = File.OpenText("input.txt").ReadToEnd().ToCharArray();
+
+for (int i = 0; i< input.Count() - 14; i++){
+    var window = input.Skip(i).Take(14);
+    if (window.Distinct().Count() == 14){
+        Console.WriteLine(i + 14);
+        break;
+    }
+}
+
+// change 14 to 4 for part1.
