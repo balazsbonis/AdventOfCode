3d67769d615f771030b4807c892ae4661e45175e
Balázs Bónis <balazs.bonis@tekenable.com>
2022-12-07 17:37:52 +0100

    [DAY03 - 04] - both

---
+++ 2022/03/Program.cs
@@ -0,0 +1,20 @@
+﻿var part01 = 0;
+var part02 = 0;
+var arr = new List<List<int>>();
+using (var str = File.OpenText("input.txt")){
+    while(!str.EndOfStream)
+    {
+        var line = str.ReadLine().ToCharArray().Select(x => (int) x - 38).Select(x => x > 59 ? x -= 58 : x);
+        var first = line.Take(line.Count() / 2);
+        part01 += line.Skip(line.Count() / 2).Intersect(first).FirstOrDefault();
+        arr.Add(line.ToList());
+        if (arr.Count == 3){
+            part02 += arr[0].Intersect(arr[1]).Intersect(arr[2]).FirstOrDefault();
+            arr = new List<List<int>>();
+        }
+    }
+}
+// part 1
+Console.WriteLine(part01);
+// part 2
+Console.WriteLine(part02);
---
+++ 2022/04/Program.cs
@@ -0,0 +1,20 @@
+﻿var part01 = 0;
+var part02 = 0;
+using (var str = File.OpenText("input.txt")){
+    while(!str.EndOfStream)
+    {
+        var line = str.ReadLine().Split(',').Select(x=>x.Split('-').Select(x=>int.Parse(x)).ToArray()).ToArray();
+        var first = Enumerable.Range(line[0][0], line[0][1] - line[0][0] + 1);
+        var second = Enumerable.Range(line[1][0], line[1][1] - line[1][0] + 1);
+        if (first.All(x => second.Contains(x)) || second.All(x => first.Contains(x))){
+            part01++;
+        }
+        if (first.Intersect(second).Any()){
+            part02++;
+        }
+    }
+}
+// part 1
+Console.WriteLine(part01);
+// part 2
+Console.WriteLine(part02);
